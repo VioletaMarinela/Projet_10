dd93483480a87d57ce154eb12befa103f61a7403
marylutza <marylutza@yahoo.com>
2024-11-21 11:58:18 +0100

    Divers, components

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <section className='footer'>
+                <p class="footer-text">Copyright 2020 Argent Bank</p>
+            </section>
+        </footer>
+    );
+};
+
+export default Footer;
